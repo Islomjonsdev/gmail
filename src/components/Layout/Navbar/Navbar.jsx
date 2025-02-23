fce1abf7ee2b3b6ecdab7930670486ecf8cb4e05
Islomjonsdev <islomjon@immigrateable.com>
2025-02-24 00:55:37 +0500

    fixed some errors

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
-import { UserButton } from '@clerk/clerk-react'
+import { useAuth, UserButton } from '@clerk/clerk-react'
 import React from 'react'
 
 const Navbar = () => {
+  const { userId } = useAuth()
+  console.log(userId);
   return (
     <div>Navbar <UserButton /></div>
   )
--- src/pages/Router/Router.jsx
+++ src/pages/Router/Router.jsx
@@ -1,8 +1,10 @@
 import Layout from "@/components/Layout/Layout";
+import { useAuth } from "@clerk/clerk-react";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
 import Login from "./Login/Login";
 import Register from "./Register/Register";
+
 
 export const router = createBrowserRouter([
   {
